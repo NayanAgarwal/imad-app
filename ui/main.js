8985a58781fbee84dbfff068046668d57a222ec3
NayanAgarwal <31137114+NayanAgarwal@users.noreply.github.com>
2017-09-04 22:49:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log('Loaded!');
+//counter code
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //create a requst object
+    var request=new XMLHttpRequest();
+    //captur the request and stores it in variable
+    if(request.readyState===XmlHttprequest.DONE){
+        //take some action
+        if(request.status==200){
+            var counter=request.reaponseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+    }
+};
+//make the request
+request.open('GET','http://agnayan.2012.imad.hasura-app.io/counter',true);
+request.send(null);
+};
